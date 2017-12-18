3ff5655761753301c0d8cedf04a69c123df85b38
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2017-12-18 13:57:27 -0600

    adds functionality to add items to the list

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
+  const submit = document.querySelector('input[type=submit]');
+
+  function toggleCheck(e){
+  	console.log(e.target)
+  }
+
+  function addItem(e){
+  	e.preventDefault();
+  	const text = (this.querySelector('[name=item]')).value;
+  	const item = {
+  		text,
+  		done: false
+  	};
+  	items.push(item);
+  	populateList(items, itemsList);
+  	this.reset();
+  }
+
+  function populateList(items = [], itemsList){
+  	itemsList.innerHTML = items.map((item, i) => {
+  		return `
+			<li>
+				<input type="checkbox" data-index=${i} id="item${i}" ${item.done ? 'checked' : ''} />
+				<label for="item${i}">${item.text}</label>
+			</li>
+  		`;
+  	}).join('');
+
+  }
+
+  addItems.addEventListener('submit', addItem);
+
   const items = [];
